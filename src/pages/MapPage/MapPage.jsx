de227b095abc1a502c80e346bd7f202898ab0b49
Mike17K <kaipismike1@gmail.com>
2023-12-20 13:25:39 +0200

    added system for items in map

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,32 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import dammyData from "./dammyData.json";
 
 // leaflet
-import { CircleMarker, MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import mapboxCredentials from "../../credentials/mapbox.credentials.json";
 
-// chartjs
-import { Chart } from "react-chartjs-2";
-
-import {
-  Chart as ChartJS,
-  LinearScale,
-  CategoryScale,
-  PointElement,
-  LineElement,
-} from "chart.js";
-
-// setup chartjs
-ChartJS.register(LinearScale, CategoryScale, PointElement, LineElement);
-
 const { mapboxAccessToken } = mapboxCredentials;
+
+// Sensor icon
+const sensorIcon = new L.Icon({
+  iconUrl: "icons/location.png", // TODO change icon
+  iconSize: [20, 20],
+});
+
+const cameraIcon = new L.Icon({
+  iconUrl: "icons/location.png", // TODO change icon
+  iconSize: [20, 20],
+});
+
+const windIcon = new L.Icon({
+  iconUrl: "icons/location.png", // TODO change icon
+  iconSize: [20, 20],
+});
 
 export default function MapPage() {
   const [searchParams, setSearchParams] = useSearchParams();
   const id = searchParams.get("id"); // use this to target sensors position
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+
+  const [treesensors, setTreesensors] = useState([]);
+  const [cameras, setCameras] = useState([]);
+  const [windsensors, setWindsensors] = useState([]);
+
+  useEffect(() => {
+    // TODO fetch data from api
+    // fetch("http://localhost:3000/treesensors")
+    //   .then((res) => res.json())
+    //   .then((data) => setTreesensors(data));
+    // fetch("http://localhost:3000/cameras")
+    //   .then((res) => res.json())
+    //   .then((data) => setCameras(data));
+    // fetch("http://localhost:3000/windsensors")
+    //   .then((res) => res.json())
+    //   .then((data) => setWindsensors(data));
+
+    // TODO remove dammy data
+    setTreesensors(dammyData.treesensors);
+    setCameras(dammyData.cameras);
+    setWindsensors(dammyData.windsensors);
+  }, []);
 
   return (
     <div className="w-[100vw] h-[100vh]">
@@ -41,6 +67,42 @@ export default function MapPage() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=${mapboxAccessToken}`}
         />
+        {treesensors.map((sensor) => (
+          <Marker
+            key={sensor.id}
+            position={[sensor.lat, sensor.lng]}
+            icon={sensorIcon}
+            eventHandlers={{
+              click: () => {
+                console.log("marker clicked");
+              },
+            }}
+          />
+        ))}
+        {cameras.map((camera) => (
+          <Marker
+            key={camera.id}
+            position={[camera.lat, camera.lng]}
+            icon={cameraIcon}
+            eventHandlers={{
+              click: () => {
+                console.log("marker clicked");
+              },
+            }}
+          />
+        ))}
+        {windsensors.map((sensor) => (
+          <Marker
+            key={sensor.id}
+            position={[sensor.lat, sensor.lng]}
+            icon={windIcon}
+            eventHandlers={{
+              click: () => {
+                console.log("marker clicked");
+              },
+            }}
+          />
+        ))}
       </MapContainer>
     </div>
   );
